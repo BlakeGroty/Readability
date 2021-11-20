ef482b98b71b27dde230a7ff61bcdc378f392f16
ZinoByte <blakegroty@gmail.com>
2021-11-20 04:17:50 -0500

    Implement readability formulas
    Finished implementation of Dale-Chall score in a manner different from what I've seen anywhere else. Will need to write docs.
    Added implementation of Flesch-Kincaid grade and Gunning Fog score. Not sure if finished; need to be verified.

--- TextAnalyzer.cs
+++ TextAnalyzer.cs
@@ -1,55 +1,73 @@
-﻿using System;
+﻿using OpenNLP.Tools.SentenceDetect;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Readability
 {
     public class TextAnalyzer
     {
+        // Constructors -------------------------------------------------------------------------------------
+        public TextAnalyzer() { }
+        public TextAnalyzer(string text)
+            => Text = text;
+
+
+        // Private helper variables -------------------------------------------------------------------------
+        private static readonly EnglishMaximumEntropySentenceDetector SentenceDetector = new EnglishMaximumEntropySentenceDetector(Settings.Default.SentenceModelLocation);
         
+        // Dale-Chall list of easy words
+        private static readonly string[] EasyWords = Resources.ChallEasyWords.Split('\n').Select(s => s.Trim()).ToArray();
+        // Suffixes to try appending to words in easyWords for a more complete list
+        private static readonly string[] Suffixes = Resources.WordSuffixes.Split('\n').Select(s => s.Trim()).ToArray();
+        // Full english dictionary -- only words found in this dictionary will be considered for difficult words
+        private static readonly string[] RealWords = File.ReadAllLines("./Resources/words.txt");
+        
+        // Public variables ---------------------------------------------------------------------------------
+        public string Text { get; set; }
 
-        private string _text;
-        public string Text
-        {
-            get => _text;
-            set
-            {
-                _text = value;
-                
-            }
-        }
-
-        public int Words { get => ; }
-        public int Sentences { get => ; }
+        public int Words { get => WordsInText.Length; }
+        public string[] WordsInText 
+        { get => Regex.Matches(Text, @"(?:\b)\w+(?:\b)").Cast<Match>().Select(m => m.Value).ToArray(); }
+        public int Sentences 
+        { get => SentenceDetector.SentenceDetect(Text).Length; }
         public double FleschKincaidGrade 
         {
-            get => 0.39 * Words / Sentences + 11.8 * TotalSyllables / Words - 15.59;
+            get
+            {
+                Debug.WriteLine($"{Words} {Sentences} {WordsInText.Sum(w => SyllableCount(w))} {WordsInText.Count(w => SyllableCount(w) >= 3)}");
+                return 0.39 * Words / Sentences + 11.8 * WordsInText.Sum(w => SyllableCount(w)) / Words - 15.59;
+            }
         }
+        // TODO: Optimize, write docs
         public double DaleChallScore
         {
             get
             {
-                string[] easyWords = Resources.ChallEasyWords.Split();
-                string[] suffixes = Resources.WordSuffixes.Split();
-                int difficultWords = WordsInText.Count(s =>
+                int difficultWords = WordsInText.Where(w => RealWords.Contains(w)).Count(s =>
                 {
-                    foreach(string suffix in suffixes)
-                        if(easyWords.Contains(s + suffix))
-                            return true;
-                    return false;
+                    s = s.ToLower();
+                    foreach(string suffix in Suffixes)
+                        foreach(string w in EasyWords)
+                            if(s.Equals(w + suffix))
+                                return false;
+                    return true;
                 });
-                
+
                 double score = 0.1579 * difficultWords / Words * 100 + 0.0496 * Words / Sentences;
                 if((double)difficultWords / Words > 0.05)
                     score += 3.6365;
                 return score;
             }
         }
-        public double DaleChallGrade
+        // TODO: Verify implementation
+        public double DaleChallGrade 
         {
             get
             {
@@ -61,17 +79,11 @@ namespace Readability
                 return 4;
             }
         }
-        public double GunningFog
-        {
-            get => 0.4 * (Words / Sentences + 100 * WordsWith3OrMoreSyllables) / Words);
-        }
+        public double GunningFog 
+        { get => 0.4 * (Words / Sentences + 100 * WordsInText.Count(w => SyllableCount(w) >= 3) / Words); }
 
-        public TextAnalyzer(string text)
-        {
-            Text = text;
-            
-        }
-
+        // Helper methods -----------------------------------------------------------------------------------
+        // TODO: Improve accuracy
         private int SyllableCount(string word)
         {
             // https://codereview.stackexchange.com/questions/9972/syllable-counting-function
