bfeff4da7403e2433810c8bd0897ba0313a098b4
Blake Groty <39283810+BlakeGroty@users.noreply.github.com>
2021-11-23 21:54:43 -0500

    Remove office dependency & reimplement algorithms (#1)
    * Removed references to Office Word and related variables
    
    Not a working build -- just meant to clear Office out
    
    * Removed trailing whitespace and added missing suffix
    
    Removed trailing whitespace from Dale-Chall word list
    Added "ed" to suffix list
    
    * Install and configure OpenNLP
    
    Installed OpenNLP
    Added settings for OpenNLP's model files
    Moved Resources.resx file to Resources/ for organization
    
    * Implement readability formulas
    
    Finished implementation of Dale-Chall score in a manner different from what I've seen anywhere else. Will need to write docs.
    Added implementation of Flesch-Kincaid grade and Gunning Fog score. Not sure if finished; need to be verified.
    
    * .gitignore fix
    
    Removed App.config and Readability.sln appearing in repo
    
    * Improve syllable counter accuracy
    
    Better handling of y
    Fixed case where e/es/ed rule could cause the number of syllables to be counted as 0
    Words ending with ia/io now get +1 syllables
    Exposed SyllableCount method to public for easier unit testing
    
    * Fix TextAnalyzer.DaleChallGrade
    
    DaleChallGrade getter now returns correct value
    Moved DaleChallGrade logic to method DaleChallGradeLevel
    DaleChallGradeLevel throws an ArgumentException for extreme values
    DaleChallGradeLevel is public to allow for easy unit testing
    
    * Add XAML docs and reformat code
    
    * Remove debug code

--- TextAnalyzer.cs
+++ TextAnalyzer.cs
@@ -1,91 +1,146 @@
-﻿using Microsoft.Office.Interop.Word;
+﻿using OpenNLP.Tools.SentenceDetect;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Readability
 {
-    class TextAnalyzer
+    public class TextAnalyzer
     {
-        private Document Doc { get; set; }
+        // Constructors -------------------------------------------------------------------------------------
+        public TextAnalyzer() { }
+        public TextAnalyzer(string text)
+            => Text = text;
 
-        private string _text;
-        public string Text
-        {
-            get => _text;
-            set
-            {
-                _text = value;
-                UpdateDoc();
-            }
-        }
 
-        public int Words { get => Doc.Words.Count; }
-        public int Sentences { get => Doc.Sentences.Count; }
-        public int Syllables 
-        {
-            get
-            {
-                // https://codereview.stackexchange.com/questions/9972/syllable-counting-function
-                string text = Text.ToLower().Trim();
-                bool lastWasVowel = false;
-                char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
-                int count = 0;
 
-                //a string is an IEnumerable<char>; convenient.
-                foreach(var c in text)
-                {
-                    if(vowels.Contains(c))
-                    {
-                        if(!lastWasVowel)
-                            count++;
-                        lastWasVowel = true;
-                    }
-                    else
-                        lastWasVowel = false;
-                }
+        // Private helper variables -------------------------------------------------------------------------
+        private static readonly EnglishMaximumEntropySentenceDetector SentenceDetector = new EnglishMaximumEntropySentenceDetector(Settings.Default.SentenceModelLocation);
+        
+        /// <summary>
+        /// Dale-Chall's "Easy words" list
+        /// </summary>
+        private static readonly string[] EasyWords = Resources.ChallEasyWords.Split('\n').Select(s => s.Trim()).ToArray();
+        /// <summary>
+        /// English word suffixes to try when looking for Dale-Chall easy words
+        /// </summary>
+        private static readonly string[] Suffixes = Resources.WordSuffixes.Split('\n').Select(s => s.Trim()).ToArray();
+        /// <summary>
+        /// Full English dictionary -- only words found in this dictionary will be considered for difficult words
+        /// </summary>
+        private static readonly string[] RealWords = File.ReadAllLines("./Resources/words.txt");
+        
 
-                if((text.EndsWith("e") || (text.EndsWith("es") || text.EndsWith("ed")))
-                      && !text.EndsWith("le"))
-                    count--;
 
-                return count;
-            }
-        }
+        // Public variables ---------------------------------------------------------------------------------
+        /// <summary>
+        /// Content to analyze. All relevant values in this class reflect the current value.
+        /// </summary>
+        public string Text { get; set; }
+
+        /// <summary>
+        /// Number of words in text
+        /// </summary>
+        public int Words { get => WordsInText.Length; }
+        
+        /// <summary>
+        /// All words in text individually
+        /// </summary>
+        public string[] WordsInText 
+        { get => Regex.Matches(Text, @"(?:\b)\w+(?:\b)").Cast<Match>().Select(m => m.Value).ToArray(); }
+        
+        /// <summary>
+        /// All sentences in text individually
+        /// </summary>
+        public int Sentences 
+        { get => SentenceDetector.SentenceDetect(Text).Length; }
+        
         public double FleschKincaidGrade 
-        {
-            get => 0.39 * Words / Sentences + 11.8 * Syllables / Words - 15.59;
-        }
-        public double DaleChallGrade
+        { get => 0.39 * Words / Sentences + 11.8 * WordsInText.Sum(w => SyllableCount(w)) / Words - 15.59; }
+        
+        // TODO: Optimize, write docs
+        public double DaleChallScore 
         {
             get
             {
-                string[] easyWords = File.ReadAllLines("./chall-easy-words.txt");
-                int difficultWords = Doc.Words.Cast<string>().Count(s => !easyWords.Contains(s));
-                double score = 0.1579 * difficultWords / Words * 100 + 0.0496 * Words / Sentences;
+                int difficultWords = WordsInText.Where(w => RealWords.Contains(w)).Count(s =>
+                {
+                    s = s.ToLower();
+                    foreach(string suffix in Suffixes)
+                        foreach(string w in EasyWords)
+                            if(s.Equals(w + suffix))
+                                return false;
+                    return true;
+                });
 
+                double score = 0.1579 * difficultWords / Words * 100 + 0.0496 * Words / Sentences;
                 if((double)difficultWords / Words > 0.05)
                     score += 3.6365;
+                return score;
             }
         }
+        
+        public double DaleChallGrade 
+        { get => DaleChallGradeLevel(DaleChallScore); }
+        
+        public double GunningFog 
+        { get => 0.4 * (Words / Sentences + 100 * WordsInText.Count(w => SyllableCount(w) >= 3) / Words); }
 
-        public TextAnalyzer(string text)
+
+
+        // Helper methods -----------------------------------------------------------------------------------
+        // TODO: Improve accuracy
+        public static int SyllableCount(string word)
         {
-            Text = text;
-            UpdateDoc();
+            // https://codereview.stackexchange.com/questions/9972/syllable-counting-function
+            word = word.ToLower().Trim();
+            bool lastWasVowel = false;
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u', 'y' };
+
+            int count = 0;
+            for(int i = 0; i < word.Length; ++i)
+            {
+                char c = word[i];
+                if(vowels.Contains(c))
+                {
+                    if(!lastWasVowel)
+                        count++;
+                    lastWasVowel = true;
+
+                    if(c == 'y' && i != 0)
+                        lastWasVowel = false;
+                }
+                else
+                    lastWasVowel = false;
+            }
+
+            if(count > 1 && (word.EndsWith("e") || word.EndsWith("es") || word.EndsWith("ed")) && !word.EndsWith("le"))
+                count--;
+
+            if(word.EndsWith("ia") || word.EndsWith("io"))
+                count++;
+
+            return count;
         }
 
-        private void UpdateDoc()
+        /// <summary>
+        /// Converts a Dale-Chall readability score to its US grade level equivalent
+        /// </summary>
+        /// <param name="score">Score to convert</param>
+        /// <returns>Approximate grade level as double</returns>
+        public static double DaleChallGradeLevel(double score)
         {
-            Application word = new Application();
-            word.Visible = false;
-            object missing = System.Reflection.Missing.Value;
-            Doc = word.Documents.Add(ref missing, ref missing, ref missing, ref missing);
-            Doc.Content.Text = Text;
+            if(score > 25 || score < -10) // Chosen arbitrarily
+                throw new ArgumentException($"Value \"{score}\" outside of tolerated range");
+
+            if(score < 5)  return 4;
+            if(score > 10) return 15;
+            return 2 * score - 5;
         }
     }
 }
